f42ab4125e79ce72666d79076b1b4828b2b137e9
Armaan7781 <armaanmulla7781@gmail.com>
2024-05-23 21:38:07 +0530

    Update Nashvill Project.sql

--- Nashvill Project.sql
+++ Nashvill Project.sql
@@ -1,85 +1,186 @@
-T20 Wc Batting scraping code: (This is just an optional code)
-/* For entire PDF, refer: https://drive.google.com/file/d/1mhjfMFZspwmt4xLZR3bemCsNj0Kkzek2/view */
-	
-/* -------------- STAGE 1 ------------ */
 
-//------- 1.a Interaction Code ------ //
-navigate('https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament');
+-- Cleaning data in SQL
+select *
+from [Portfolio Projects]..[NashvillHousing]
 
+-------------------------------------------------------------------------------------
 
-let links = parse().matchSummaryLinks;
-for(let i of links) { 
-  next_stage({url: i}) 
-}
-
-//------- 1.b Parser Code ------------//
-let links = []
-const allRows = $('table.engineTable > tbody > tr.data1');
- allRows.each((index, element) => {
-  const tds = $(element).find('td');
-  const rowURL = "https://www.espncricinfo.com" +$(tds[6]).find('a').attr('href');
-  links.push(rowURL);
- })
-return {
-  'matchSummaryLinks': links
-};
-
-/* -------------- STAGE 2 ------------ */
-
-//------- 2.a Interaction Code ------ //
-navigate(input.url);
-collect(parse());
-
-//------- 2.b Parser Code ------------//
-var match = $('div').filter(function(){
-	return $(this)
-      .find('span > span > span').text() === String("Match Details") 
-}).siblings()
-team1 = $(match.eq(0)).find('span > span > span').text().replace(" Innings", "")
-team2 = $(match.eq(1)).find('span > span > span').text().replace(" Innings", "")
-matchInfo = team1 +  ' Vs ' + team2
-
-var tables = $('div > table.ci-scorecard-table');
-var firstInningRows = $(tables.eq(0)).find('tbody > tr').filter(function(index, element){
-  return $(this).find("td").length >= 8
-})
-
-var secondInningsRows = $(tables.eq(1)).find('tbody > tr').filter(function(index, element){
-  return $(this).find("td").length >= 8
-});
+-- Standardize Date Format
 
 
-var battingSummary = []
-firstInningRows.each((index, element) => {
-  var tds = $(element).find('td');
-  battingSummary.push({
-  		"match": matchInfo,
-  		"teamInnings": team1,
-   		"battingPos": index+1,
-  		"batsmanName": $(tds.eq(0)).find('a > span > span').text().replace(' ', ''),
-    	"dismissal": $(tds.eq(1)).find('span > span').text(),
-  		"runs": $(tds.eq(2)).find('strong').text(), 
-  		"balls": $(tds.eq(3)).text(),
-  		"4s": $(tds.eq(5)).text(),
-  		"6s": $(tds.eq(6)).text(),
- 		"SR": $(tds.eq(7)).text()
-  });
-});
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Sale Date] date;
 
-secondInningsRows.each((index, element) => {
-  var tds = $(element).find('td');
-   battingSummary.push({
-  		"match": matchInfo,
-  		"teamInnings": team2,
-   		"battingPos": index+1,
-  		"batsmanName": $(tds.eq(0)).find('a > span > span').text().replace(' ', ''),
-     	"dismissal": $(tds.eq(1)).find('span > span').text(),
-  		"runs": $(tds.eq(2)).find('strong').text(), 
-  		"balls": $(tds.eq(3)).text(),
-  		"4s": $(tds.eq(5)).text(),
-  		"6s": $(tds.eq(6)).text(),
- 		"SR": $(tds.eq(7)).text()
-  });
-});
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Sale Date] = Convert(Date,SaleDate)
 
-return {"battingSummary": battingSummary}
+Select [Sale Date]
+from [PORTFOLIO Project ]..Nashvillhousing  
+
+
+-------------------------------------------------------------------------------------------------
+
+-- Populate Property Address Data 
+
+Select *
+from [PORTFOLIO Project ]..Nashvillhousing
+--where PropertyAddress is null
+order by ParcelID
+
+Select A.ParcelID,A.PropertyAddress,B.ParcelID, B.PropertyAddress, ISNULL(A.PropertyAddress,B.PropertyAddress)
+from [PORTFOLIO Project ]..Nashvillhousing A
+JOIN [PORTFOLIO Project ]..Nashvillhousing B
+	ON A.ParcelID = B.ParcelID
+	AND A.UniqueID <> B.UniqueID
+Where A.PropertyAddress is null
+
+update A
+set propertyaddress = ISNULL(A.PropertyAddress,B.PropertyAddress)
+from [PORTFOLIO Project ]..Nashvillhousing A
+JOIN [PORTFOLIO Project ]..Nashvillhousing B
+	ON A.ParcelID = B.ParcelID
+	AND A.UniqueID <>B.UniqueID
+
+--------------------------------------------------------------------------------
+
+-- Breaking out address into individual columns (Address, City, State)
+
+Select PropertyAddress
+from [Portfolio Project]..NashvillHousing
+
+select 
+substring(PropertyAddress, 1, charindex(',',PropertyAddress)-1) as Address,
+substring(PropertyAddress,charindex(',',PropertyAddress)+1, len(PropertyAddress))as Address
+from [Portfolio Project]..NashvillHousing
+
+
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Property_Split Address] nvarchar (255);
+
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Property_Split Address] = substring(PropertyAddress, 1, charindex(',',PropertyAddress)-1)
+
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Property_Split City] nvarchar(255);
+
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Property_Split City] = substring(PropertyAddress,charindex(',',PropertyAddress) +1, len(PropertyAddress))
+
+
+Select [Property_Split City],[Property Split Address]
+from [PORTFOLIO Project ]..Nashvillhousing 
+
+
+
+SELECT *
+FROM [PORTFOLIO Project ]..Nashvillhousing
+
+
+Select ownerAddress
+from [PORTFOLIO Project ]..Nashvillhousing
+
+
+select 
+PARSENAME(REPLACE(OwnerAddress,',', '.') ,3)
+,PARSENAME(REPLACE(OwnerAddress,',', '.') ,2)
+,PARSENAME(REPLACE(OwnerAddress,',', '.') ,1)
+FROM [PORTFOLIO Project ]..Nashvillhousing
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Owner_Split Address] nvarchar (255);
+
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Owner_Split Address] = PARSENAME(REPLACE(OwnerAddress,',', '.') ,3)
+
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Owner_Split City] nvarchar(255);
+
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Owner_Split City] = PARSENAME(REPLACE(OwnerAddress,',', '.') ,2)
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+add [Owner_Split State] nvarchar(255);
+
+update [PORTFOLIO Project ]..Nashvillhousing 
+set [Owner_Split State] = PARSENAME(REPLACE(OwnerAddress,',', '.') ,1)
+
+
+--Alter table [PORTFOLIO Project ]..Nashvillhousing 
+--drop column [Owner_Split Address]
+
+
+------------------------------------------------------------------------------------------------------------
+
+-- Change Y and N to Yes And NO in'Sold in vacant' field
+
+
+select distinct(soldasvacant),count(soldasvACANT)
+from [PORTFOLIO Project ]..Nashvillhousing  
+GROUP BY SOLDASVACANT
+ORDER BY 2
+
+
+SELECT SOLDASVACANT,
+	CASE 
+		when soldasvacant ='Y' Then 'Yes'
+		When soldasvacant = 'N' Then 'No'
+		Else 
+		SoldAsVacant
+	END
+from  [PORTFOLIO Project ]..Nashvillhousing 
+
+
+UPDATE NashvillHousing
+set SoldAsVacant = 	CASE 
+		when soldasvacant ='Y' Then 'Yes'
+		When soldasvacant = 'N' Then 'No'
+		Else 
+		SoldAsVacant
+	END
+
+
+	--------------------------------------------------------------------------------------------------------------
+
+-- Remove Duplicates
+
+
+
+
+WITH ROW_NUM_CTE AS(
+select *, 
+		ROW_NUMBER() OVER (
+		PARTITION BY PARCELID,
+					 PROPERTYaDDRESS,
+					 SALEPRICE,
+					 SALEDATE,
+					 LegalReference
+					 order by 
+						UniqueID
+						) ROW_NUM
+
+
+from  [PORTFOLIO Project ]..Nashvillhousing
+--ORDER BY PARCELID
+)
+DELETE 
+FROM ROW_NUM_CTE
+WHERE ROW_NUM> 1
+--ORDER BY PROPERTYADDRESS
+
+------------------------------------------------------------------------------------------------------------------------
+
+--Delete Unused Columns
+
+select *
+from  [PORTFOLIO Project ]..Nashvillhousing 
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+DROP COLUMN owneraddress, TaxDistrict, PropertyAddress
+
+
+alter table [PORTFOLIO Project ]..Nashvillhousing 
+drop column SaleDate
+
